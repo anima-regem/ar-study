24aa112ee1212aacdcf7625683d41d60a244cbf5
anima-regem <vichukartha@gmail.com>
2023-05-14 22:04:01 +0530

    first commit

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1710548.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 128108352, 128108349, 128125357 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
